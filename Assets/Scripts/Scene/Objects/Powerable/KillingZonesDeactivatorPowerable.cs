7c3b9c07256970cf83ac8e979fa76809b299c0f7
OrloKun <orlocuario@gmail.com>
2018-03-12 15:51:02 -0300

    fixing mageGearPath

---
+++ Assets/Scripts/Scene/Objects/Powerable/KillingZonesDeactivatorPowerable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingZonesDeactivatorPowerable : PowerableObject {
+
+    public KillingObject[] killingObjects;
+
+    protected override void DoYourPowerableThing()
+    { 
+        foreach (KillingObject kObject in killingObjects)
+        {
+            kObject.SetActive(false);
+        }
+
+    }
+
+    protected override void UndoYourPowerableThing()
+    {
+        foreach (KillingObject kObject in killingObjects)
+        {
+            kObject.SetActive(true);
+        }
+    }
+
+
+}
